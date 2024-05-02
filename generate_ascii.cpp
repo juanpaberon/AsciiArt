dddccb60e99bde03c87d340edeab67b3c2be30c4
jberon3 <jberon3@gatech.edu>
2024-05-02 11:58:33 -0500

    change font and size

--- generate_ascii.cpp
+++ generate_ascii.cpp
@@ -17,12 +17,12 @@ int main() {
 
 	Mat img(500, 500, CV_8UC3, Scalar(0, 0, 0));
 
-	int jump = 10;
+	int jump = 8;
 
 	cout << input_img.channels() << endl;
 
-	for (int i = 0; i < 500; i+=jump) {
-		for (int j = 0; j < 500; j+=jump) {
+	for (int i = 0; i < 500-jump+1; i+=jump) {
+		for (int j = 0; j < 500-jump+1; j+=jump) {
 			Range rows(i, i + jump);
 			Range cols(j, j + jump);
 			int mean = floor((sum(input_img(rows, cols))[0]) / (jump * jump));
@@ -30,9 +30,9 @@ int main() {
 			double tmp = (mean / 255.0) * letters.size();
 			int letter_selector = round(tmp);
 
-			Point org(j, i);
+			Point org(j+jump/2, i+jump/2);
 			string selected_letter(1, letters[letter_selector]);
-			putText(img, selected_letter, org, FONT_HERSHEY_SIMPLEX, 0.3, Scalar(255, 255, 255), 1);
+			putText(img, selected_letter, org, FONT_HERSHEY_PLAIN, 0.4, Scalar(255, 255, 255), 1);
 		}
 	}
 
