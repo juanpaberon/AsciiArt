e5cb6050c448fa0c25ab6c43f628136eae7f7daa
jberon3 <jberon3@gatech.edu>
2024-05-08 15:53:06 -0500

    stop hardcoding the size of the image

--- generate_ascii.cpp
+++ generate_ascii.cpp
@@ -33,22 +33,24 @@ int findBiggestSize(string letters, double font_size) {
 int main() {
 
 	string letters = " _.,-++:lcba!?0123456789$#@Ñ";
-	double font_size = 0.4;
+	double font_size = 0.5;
 
 	cout << findBiggestSize(letters, font_size) << endl;
 
-	string img_path = "input_images\\img.png";
+	string img_path = "input_images\\img.jpeg";
 	Mat input_img;
 	cvtColor(imread(img_path, IMREAD_COLOR), input_img, COLOR_BGR2GRAY);
 
-	Mat img(500, 500, CV_8UC3, Scalar(0, 0, 0));
+	int width = input_img.size[1];
+	int height = input_img.size[0];
+	Mat img(height, width, CV_8UC3, Scalar(0, 0, 0));
 
 	int jump = findBiggestSize(letters, font_size);
 
 	cout << input_img.channels() << endl;
 
-	for (int i = 0; i < 500-jump+1; i+=jump) {
-		for (int j = 0; j < 500-jump+1; j+=jump) {
+	for (int i = 0; i < height-jump+1; i+=jump) {
+		for (int j = 0; j < width-jump+1; j+=jump) {
 			Range rows(i, i + jump);
 			Range cols(j, j + jump);
 			int mean = floor((sum(input_img(rows, cols))[0]) / (jump * jump));
@@ -58,7 +60,7 @@ int main() {
 
 			Point org(j+jump/2, i+jump/2);
 			string selected_letter(1, letters[letter_selector]);
-			putText(img, selected_letter, org, FONT_HERSHEY_PLAIN, 0.4, Scalar(255, 255, 255), 1);
+			putText(img, selected_letter, org, FONT_HERSHEY_PLAIN, font_size, Scalar(255, 255, 255), 1);
 		}
 	}
 
